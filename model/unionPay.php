f9a1f8496e95944c5c2449d6215d7515102bf043
developersPHP <15219740206@163.com>
2016-08-30 16:55:12 +0800

    Update unionPay.php
    you can wirte some logic into the function

--- model/unionPay.php
+++ model/unionPay.php
@@ -1,7 +1,24 @@
 <?php
 include_once('../Pay.php');
-class unionPay
+class unionPay extends Pay
 {
+  private $keys;
+
+  public function __construct() {
+    
+  }
   //此处处理银联支付类
+  public function sign() {
+    
+  }
   
+  //http请求
+  public function request() {
+    
+  }
+  
+  //获取数据展示
+  public function show() {
+    
+  }
 }
