29bbdb0005cef07a4ca274d86e108f50e8944341
developersPHP <15219740206@163.com>
2016-09-05 15:11:36 +0800

    Update sign.php

--- lib/sign.php
+++ lib/sign.php
@@ -1,24 +1,37 @@
 <?php
-include_once('./Serializableinterface.php');
-define(SDK_SIGN_CERT_PATH,'/data/golo/ext/Unionpay/certificate/config/renbao/test/PM_700000000000001_acp.pfx')
-class sign 
+namespace PayCenter\lib;
+use PayCenter\lib\showResult;
+//define(SDK_SIGN_CERT_PATH,'/data/golo/ext/Unionpay/certificate/config/renbao/work/renbao_privatekey.pfx');
+define(SDK_SIGN_CERT_PATH,'D:\Platform\dev\ext\Unionpay\certificate\config\renbao\work\renbao_privatekey.pfx');
+define(SDK_SIGN_CERT_PWD,'rb4231');
+class sign
 {
-  //此类用于解决支付接口签名问题
-  public function sign($param) {
-    
-    //参数转成key=value&形式
-    $str_param = serializeParam($param);
-    $ha1x16_param = sha1($str_param,FALSE);
-    
     //签名证书路径
-    $cert_path = SDK_SIGN_CERT_PATH;
-    //获取私钥
-    $private_key_certificate = file_get_contents($cert_path);
-    openssl_pkcs12_read($private_key_certificate,$private_key,'rb4231');
-    
-    //获取签名
-    $sign_flag = openssl_sign($ha1x16_param,$signature,$private_key['pkey'],1);
-    
-    $sign = $fign_flag ? base64_encode ( $signature ):'';
-  }
+    public static function getPrivateKey($cert_path) {
+
+        $pkcs12 = file_get_contents ( $cert_path );
+        openssl_pkcs12_read ( $pkcs12, $certs, SDK_SIGN_CERT_PWD );
+        return $certs ['pkey'];
+    }
+    //获取
+    public function getSignCertId()
+    {
+        // 签名证书路径
+        return self::getCertId(SDK_SIGN_CERT_PATH);
+    }
+
+    /**
+     * 取证书ID(.pfx)
+     *
+     * @return unknown
+     */
+    public function getCertId($cert_path)
+    {
+        $pkcs12certdata = file_get_contents($cert_path);
+        openssl_pkcs12_read($pkcs12certdata, $certs, SDK_SIGN_CERT_PWD);
+        $x509data = $certs['cert'];
+        openssl_x509_read($x509data);
+        $certdata = openssl_x509_parse($x509data);
+        return $certdata['serialNumber'];
+    }
 }
