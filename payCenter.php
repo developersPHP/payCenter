7539028e5f0af12113ce6bf33e8df6fada8843e2
developersPHP <15219740206@163.com>
2016-09-05 09:33:53 +0800

    Update payCenter.php

--- payCenter.php
+++ payCenter.php
@@ -2,7 +2,7 @@
 abstract class PayCenter
 {
   //此处处理不同的支付类对象
-  procted abstract function payMethod(Pay $diffrent_method);
+  protected abstract function payMethod(Pay $diffrent_method);
   
   public function startPay($payMethod) {
     $result = $this->payMethod($payMethod);
