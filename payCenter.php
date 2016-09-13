bd224322d30c1d178b4fba2555c3328de9a0c358
David.Liu <15219740206@163.com>
2016-09-13 15:03:06 +0800

    Update payCenter.php

--- payCenter.php
+++ payCenter.php
@@ -5,7 +5,7 @@ use PayCenter\PayInterface;
 abstract class PayCenter
 {
     //此处处理不同的支付类对象
-    protected  abstract function payMethod(PayFactory $different_method);
+    protected  abstract function payMethod(PayInterface $different_method);
 
     public function startPay($payMethod) {
         $result = $this->payMethod($payMethod);
